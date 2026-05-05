ec8f8ccaab445cf058fea07714079b955b35d5c8
Andro-hazem <androhazem@gmail.com>
2026-05-06 00:28:58 +0300

    changing functions to classes

--- budgets/models.py
+++ budgets/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-from users.models import CustomUser
+from django.conf import settings
 
 
 class Budget(models.Model):
-    user            = models.ForeignKey("users.CustomUser", on_delete=models.CASCADE)
+    user            = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     category        = models.ForeignKey("transactions.Category", on_delete=models.CASCADE)
     amount          = models.DecimalField(max_digits=10, decimal_places=2)
     start_date      = models.DateField()
