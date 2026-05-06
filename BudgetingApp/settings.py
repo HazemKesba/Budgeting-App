2953201a9e35dbba6536d3744b02e7cbff24aee3
HazemKesba <hazemkesba@gmail.com>
2026-05-07 02:18:51 +0300

    Added new default login url

--- BudgetingApp/settings.py
+++ BudgetingApp/settings.py
@@ -95,6 +95,9 @@ AUTH_USER_MODEL = "users.CustomUser"
 # Login redirect URL
 LOGIN_REDIRECT_URL = "/dashboard/"
 
+# Login URL
+LOGIN_URL = 'login'
+
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
